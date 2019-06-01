719449096c78e72afb0e2199956c9f2e31c48a78
Mike Huitrado <huitrado.mike@gmail.com>
2019-06-01 12:46:36 -0300

    feat: Add user input service

--- video-maker.py
+++ video-maker.py
@@ -1,11 +1,13 @@
 from Content import Content
+from services import UserInput
 
 print("Starting orchestrator")
 
 content = Content()
 
 # Read user input
-
+content = UserInput.read_input_and_update_content(content)
+print(content.to_json())
 # Fetch content from source
 
 # Process content text
